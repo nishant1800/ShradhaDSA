84dd7cd27f22b04201179fa0e905a657ac8326ca
Nishant <ranjannishant18011@gmail.com>
2025-08-11 01:14:00 +0530

    majorityelement

--- Arrays/MajorityElement.java
+++ Arrays/MajorityElement.java
@@ -1,7 +1,22 @@
-// 169. Leetcode
+// 169. Majority Element
 
-public class MajorityElement {
-    public static void main(String args[]) {
-        
+class Solution {
+    public int majorityElement(int[] nums) {
+        int cand = 0;
+        int count = 0;
+
+        for(int i=0; i<nums.length; i++) {
+            if(count == 0) {
+                cand = nums[i];
+            }
+
+            if(cand == nums[i]) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return cand;
     }
 }
