8a02cbde340fe06764c53428e5f32d8377a29768
Nishant <ranjannishant18011@gmail.com>
2025-09-11 23:28:04 +0530

    majority element

--- Arrays/MajorityElement.java
+++ Arrays/MajorityElement.java
@@ -1,22 +1,25 @@
 // 169. Majority Element
 
+import java.util.HashMap;
+
 class Solution {
     public int majorityElement(int[] nums) {
-        int cand = 0;
-        int count = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
 
         for(int i=0; i<nums.length; i++) {
-            if(count == 0) {
-                cand = nums[i];
-            }
-
-            if(cand == nums[i]) {
-                count++;
+            if(map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i])+1);
             } else {
-                count--;
+                map.put(nums[i], 1);
             }
         }
 
-        return cand;
+        for(int i=0; i<nums.length; i++) {
+            if(map.get(nums[i]) > nums.length/2) {
+                return nums[i];
+            }
+        }
+
+        return -1;
     }
 }
