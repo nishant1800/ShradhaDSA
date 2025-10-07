17ca30b2c9807aaa357278129009d08938a50bfc
NishantRanjan <ranjannishant18011@gmail.com>
2025-10-07 17:10:49 +0530

    leetcode 151

---
+++ Strings/ReverseWordsinaString.java
@@ -0,0 +1,18 @@
+// 151. Reverse Words in a String
+
+class Solution {
+    public String reverseWords(String s) {
+        String words[] = s.trim().split("\\s+");
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=words.length-1; i>=0; i--) {
+            sb.append(words[i]);
+
+            if(i != 0) {
+                sb.append(" ");
+            }
+        }
+
+        return sb.toString();
+    }
+}
