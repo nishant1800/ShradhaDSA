3d00af0fc71c1be902c52d9e160a57f898882074
NishantRanjan <ranjannishant18011@gmail.com>
2025-10-08 13:19:34 +0530

    leetcode 1910

---
+++ Strings/RemoveAllOccurrencesofaSubstring.java
@@ -0,0 +1,12 @@
+// 1910. Remove All Occurrences of a Substring
+
+class Solution {
+    public String removeOccurrences(String s, String part) {
+        while(s.contains(part)) {
+            int index = s.indexOf(part);
+            s = s.substring(0, index) + s.substring(index + part.length());
+        }
+
+        return s;
+    }
+}
